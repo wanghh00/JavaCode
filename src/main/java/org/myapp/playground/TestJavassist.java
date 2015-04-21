3dc7e07fbe54ed856d71b01b58638ba9748b35cf
Honghao Wang <hongwang@ebay.com>
2015-04-20 21:55:12 -0400

    code style changes

--- src/main/java/org/myapp/playground/TestJavassist.java
+++ src/main/java/org/myapp/playground/TestJavassist.java
@@ -9,7 +9,7 @@ import javassist.NotFoundException;
 
 public class TestJavassist {
 	public static int STACK_TRACE_DEP = 2;
-	
+
 	public static void chkMethodByStackTrace() {
 		Method method = null;
 		StackTraceElement stack = Thread.currentThread().getStackTrace()[STACK_TRACE_DEP];
@@ -19,11 +19,11 @@ public class TestJavassist {
 
 		try {
 			method = Class.forName(className).getMethod(methodName, int.class);
-			
+
 			for (Method m : Class.forName(className).getDeclaredMethods()) {
 				System.out.println(m.getName() + " " + m.getParameterTypes());
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -32,41 +32,41 @@ public class TestJavassist {
 	public static void dummy() {
 		chkMethodByStackTrace();
 	}
-	
+
 	public static void dummy(int x) {
 		chkMethodByStackTrace();
 	}
 
 	public static void chkMethodByJavassit() throws Exception {
 		String methodName = "dummy";
-		
+
 		System.out.println("Get method line number with javassist\n");
 
 		ClassPool pool = ClassPool.getDefault();
 		CtClass cc = pool.get("org.myapp.playground.TestJavassist");
-//		CtMethod methodX = cc.getDeclaredMethod("dummy");
-//		int xlineNumber = methodX.getMethodInfo().getLineNumber(1);
-		
+		// CtMethod methodX = cc.getDeclaredMethod("dummy");
+		// int xlineNumber = methodX.getMethodInfo().getLineNumber(1);
+
 		for (CtMethod m : cc.getDeclaredMethods()) {
-			//System.out.println("LET ME SEE: " + m.getName());
-			
-			//String nm = m.getName();
-			//if (nm.equals(methodName)) System.out.println("HAHA...");
-			
-			if (! "dummy".equals(m.getName())) continue; 
-//			m.getMethodInfo().getLineNumber(0);
-			
+			// System.out.println("LET ME SEE: " + m.getName());
+
+			// String nm = m.getName();
+			// if (nm.equals(methodName)) System.out.println("HAHA...");
+
+			if (!"dummy".equals(m.getName())) continue;
+			// m.getMethodInfo().getLineNumber(0);
+
 			System.out.println(methodName + ": " + m.getMethodInfo().getLineNumber(0));
 		}
 
-//		System.out.println("method x is on line " + xlineNumber + "\n");
+		// System.out.println("method x is on line " + xlineNumber + "\n");
 
 	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		chkMethodByJavassit();
-		//dummy(1);
+		// dummy(1);
 	}
 
 }
--- src/main/java/org/myapp/util/LoadYamlPropertyFile.java
+++ src/main/java/org/myapp/util/LoadYamlPropertyFile.java
@@ -30,7 +30,7 @@ public class LoadYamlPropertyFile {
 		}
 		return data;
 	}
-	
+
 	public String retPropertyFileIOUtils(String filename, String encodeName) {
 		InputStream in = getClass().getClassLoader().getResourceAsStream(filename);
 		String data = null;
@@ -49,7 +49,7 @@ public class LoadYamlPropertyFile {
 		BufferedReader reader = null;
 		String line = null;
 		StringBuilder total = new StringBuilder();
-		
+
 		try {
 			if (encodeName == null) encodeName = "UTF8";
 			reader = new BufferedReader(new InputStreamReader(in, encodeName));
@@ -60,27 +60,27 @@ public class LoadYamlPropertyFile {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			//IOUtils.closeQuietly(reader);
+			// IOUtils.closeQuietly(reader);
 			try {
 				if (reader != null) reader.close();
 			} catch (IOException e) {}
 		}
 		return total.toString();
 	}
-	
+
 	public static String joinStringList_old(List<String> list, String delimiter) {
 		StringBuilder sb = new StringBuilder();
 		for (String str : list) {
 			if (delimiter == null) delimiter = "";
-			sb.append(str+delimiter);
+			sb.append(str + delimiter);
 		}
 		return sb.toString();
 	}
-	
+
 	public static String joinStringList(List<String> list, String delimiter) {
 		StringBuilder sb = new StringBuilder();
 		Iterator<String> iter = list.iterator();
-		
+
 		if (iter.hasNext()) sb.append(iter.next());
 		while (iter.hasNext()) {
 			if (delimiter == null) delimiter = "";
@@ -88,62 +88,64 @@ public class LoadYamlPropertyFile {
 		}
 		return sb.toString();
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	protected void travelMap(Map<String, Object> map, int level, Vector<String> vctName, Map<String, String> mpRet) {		
-		for (Map.Entry<String, Object> entry: map.entrySet()) {
+	protected void travelMap(Map<String, Object> map, int level, Vector<String> vctName, Map<String, String> mpRet) {
+		for (Map.Entry<String, Object> entry : map.entrySet()) {
 			vctName.set(level, entry.getKey());
 			if (entry.getValue() instanceof Map) {
-				travelMap((Map<String, Object>)entry.getValue(), level+1, vctName, mpRet);
+				travelMap((Map<String, Object>) entry.getValue(), level + 1, vctName, mpRet);
 				continue;
-			}			
-			mpRet.put(joinStringList(vctName,"!")+"!", (String)entry.getValue());
-		}		
+			}
+			mpRet.put(joinStringList(vctName, "!") + "!", (String) entry.getValue());
+		}
 	}
-	
+
 	public void genFromMap(Map<String, Object> map) {
-		Vector<String> vctName = new Vector<String>(Arrays.asList("","",""));
+		Vector<String> vctName = new Vector<String>(Arrays.asList("", "", ""));
 		Map<String, String> mpRet = new HashMap<String, String>();
 		int level = 0;
-		
+
 		travelMap(map, level, vctName, mpRet);
 		for (Map.Entry<String, String> entry : mpRet.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void loadYaml() {
 		String filename = "org/myapp/playground/properties.yaml";
-		
-		Yaml yaml = new Yaml();		
+
+		Yaml yaml = new Yaml();
 		Map<String, Object> mpYaml = (Map<String, Object>) yaml.load(retPropertyFile(filename, null));
 		System.out.println(mpYaml);
-		
+
 		genFromMap(mpYaml);
-		
+
 	}
-	
+
 	public static List<String> splitString(String str, String delimiter) {
-		String [] array = str.split(delimiter);
+		String[] array = str.split(delimiter);
 		return Arrays.asList(array);
 	}
-	
+
 	public static void main(String[] args) {
-		
-		// InputStream in = this.getClass().getResourceAsStream("/com/ebay/playground/TodayLactors.yaml");
+
+		// InputStream in =
+		// this.getClass().getResourceAsStream("/com/ebay/playground/TodayLactors.yaml");
 		LoadYamlPropertyFile ly = new LoadYamlPropertyFile();
-		
+
 		ly.loadYaml();
-		
+
 		List<String> list = splitString("AA,BB,CC", ",");
 		System.out.println(list.getClass().getName());
-		
-		for (String str: splitString("AA,BB,CC", ",")) {
+
+		for (String str : splitString("AA,BB,CC", ",")) {
 			System.out.println(str);
 		}
-		
-		//Vector<String> vctName = new Vector<String>(Arrays.asList("a","b","c"));
-		//System.out.println(joinStringListIterator(vctName,"!"));		
+
+		// Vector<String> vctName = new
+		// Vector<String>(Arrays.asList("a","b","c"));
+		// System.out.println(joinStringListIterator(vctName,"!"));
 	}
 }
